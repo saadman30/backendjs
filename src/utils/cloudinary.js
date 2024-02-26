1907e7e7a579eab0b884727bfbd94f19b1ab0ec2
saadman30 <saadman.ofc@gmail.com>
2024-02-26 18:08:35 +0600

    delete locally saved uploaded files

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file has been uploaded sucessfully
-    console.log("file has been uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath)
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload opertion got failed
